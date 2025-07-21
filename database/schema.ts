38caaa745d79b41d782924a0d9a7d9761476c91e
Kshitiz <kshitizs2008@gmail.com>
2025-07-21 06:04:36 +0530

    db setup neon+drizzle

---
+++ database/schema.ts
@@ -0,0 +1,19 @@
+import { pgTable, integer, text, timestamp } from 'drizzle-orm/pg-core';
+
+export const contactsTable = pgTable('contacts_table', {
+  id: integer('id').primaryKey().notNull(),
+  phoneNumber: text('phone_number'),
+  email: text('email'),
+  linkedId: integer('linked_id'),
+  linkPrecedence: text('link_precedence', { enum: ['primary', 'secondary'] }).notNull(),
+  createdAt: timestamp('created_at').notNull(),
+  updatedAt: timestamp('updated_at').notNull(),
+  deletedAt: timestamp('deleted_at'),
+});
+
+export type InsertUser = typeof contactsTable.$inferInsert;
+export type SelectUser = typeof contactsTable.$inferSelect;
+
+
+
+
